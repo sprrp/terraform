b123eb6ac615a645bf562723a0a4afcf75c23053
sprrp@outlook.com <sprrp@outlook.com>
2021-04-23 12:52:49 +0000

    adding for ec2

--- west/es.tf
+++ west/es.tf
@@ -19,8 +19,8 @@ resource "aws_elasticsearch_domain" "es" {
 
   vpc_options {
     subnet_ids = [
-      data.aws_subnet_ids.default.ids[0],
-      data.aws_subnet_ids.default.ids[1],
+      data.aws_subnet_ids.default.ids,
+      data.aws_subnet_ids.default.ids
     ]
 
     security_group_ids = [aws_security_group.default.id]
