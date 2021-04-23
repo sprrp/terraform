37f9288ea7eb917330daea0e0a0f1e4c902118cc
sprrp@outlook.com <sprrp@outlook.com>
2021-04-23 12:54:28 +0000

    adding for ec2

--- west/es.tf
+++ west/es.tf
@@ -1,7 +1,7 @@
 data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
-data "aws_subnet_ids" "default" {
+data "aws_subnet" "default" {
   vpc_id = data.aws_vpc.default.id
 
   tags = {
