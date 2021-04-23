646707265db74f6badc1e050c1fccc02ae1e0f6d
sprrp@outlook.com <sprrp@outlook.com>
2021-04-23 13:29:18 +0000

    fix bug

--- west/sgs.tf
+++ west/sgs.tf
@@ -1,9 +1,11 @@
-data "aws_vpc" "default" {}
+data "aws_vpc" "default" {
+
+}
 
 resource "aws_security_group" "default" {
   name        = "surendra-sg"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.default.id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     description      = "TLS from VPC"
