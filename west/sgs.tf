5ff093bec2ee805bcaefa652ce95b2d9ee6654ef
sprrp@outlook.com <sprrp@outlook.com>
2021-04-23 13:24:04 +0000

    fix bug

--- west/sgs.tf
+++ west/sgs.tf
@@ -1,28 +1,25 @@
-  data "aws_vpc" "default" {}
+data "aws_vpc" "default: {}
 
 resource "aws_security_group" "default" {
-  name        = "allow_tls"
+  name        = "surendra-sg"
   description = "Allow TLS inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.default.id
 
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-   # cidr_blocks      = ["0.0.0.0/0"]
-   # ipv6_cidr_blocks = ["::/0"]
     cidr_blocks      = [aws_vpc.default.cidr_block]
-   # ipv6_cidr_blocks = [aws_vpc.default.ipv6_cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.default.ipv6_cidr_block]
   }
-  
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-   # ipv6_cidr_blocks = ["::/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
