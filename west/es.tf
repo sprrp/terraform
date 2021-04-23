7a96527981340390a09d9d5e6a7ac06903d9e680
sprrp@outlook.com <sprrp@outlook.com>
2021-04-23 12:47:47 +0000

    adding for ec2

---
+++ west/es.tf
@@ -0,0 +1,56 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+
+  tags = {
+    Tier = "private"
+  }
+}
+
+resource "aws_elasticsearch_domain" "es" {
+  domain_name           = var.domain
+  elasticsearch_version = "7.9"
+
+  cluster_config {
+    instance_type = "t2.micro.elasticsearch"
+  }
+
+  vpc_options {
+    subnet_ids = [
+      data.aws_subnet_ids.default.ids[0],
+      data.aws_subnet_ids.default.ids[1],
+    ]
+
+    security_group_ids = [aws_security_group.es.id]
+  }
+
+  advanced_options = {
+    "rest.action.multi.allow_explicit_index" = "true"
+  }
+
+  access_policies = <<CONFIG
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "es:*",
+            "Principal": "*",
+            "Effect": "Allow",
+            "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${var.domain}/*"
+        }
+    ]
+}
+CONFIG
+
+  snapshot_options {
+    automated_snapshot_start_hour = 23
+  }
+
+  tags = {
+    Domain = "TestDomain"
+  }
+
+  depends_on = [aws_iam_service_linked_role.es]
+}
