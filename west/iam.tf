fe67f773e63648477e2edcf96a3863aa5744a25d
sprrp <61840926+sprrp@users.noreply.github.com>
2021-04-28 09:40:13 -0700

    Update iam.tf

--- west/iam.tf
+++ west/iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_service_linked_role" "main" {
-  default     = "true"
+resource "aws_iam_service_linked_role" "es" {
+  create_iam_service_linked_role = false
  aws_service_name = "es.amazonaws.com"
 }
